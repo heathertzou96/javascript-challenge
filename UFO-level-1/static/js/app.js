867332126437e9f755a3be5be820f534c56fc7b9
Heather Tzou <60579446+heathertzou96@users.noreply.github.com>
2020-05-18 15:10:20 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,28 +3,12 @@ var tableData = data;
 
 // YOUR CODE HERE!
 // looping through array of objects and printing them out in console
-// tableData.forEach(function(ufo) {
-//     console.log(ufo);
-// });
+tableData.forEach(function(ufo) {
+    console.log(ufo);
+});
 
 // appending table to webpage and adding new rows of data for each UFO sighting
-
-// function init() {
-    
-//     var tbody = d3.select("tbody"); 
-
-//     tableData.forEach((ufo) => {
-//         var row = tbody.append("tr");
-//         Object.entries(ufo).forEach(([key, value]) => {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         }); 
-//     });
-// }
-
 var tbody = d3.select("tbody"); 
-
-
 
 tableData.forEach((ufo) => {
     var row = tbody.append("tr");
@@ -33,8 +17,6 @@ tableData.forEach((ufo) => {
         cell.text(value);
     }); 
 });
-
-
 
 // listening for event based on date input
 var button = d3.select("#filter-btn");
@@ -60,34 +42,7 @@ function filterTable() {
             var cell = row.append("td");
             cell.text(value);
         });
-    });
-    //updateTable(filteredData);
-    
-    //return filteredData;
-    //console.log(filteredData);
-    
+    });  
 };
-
-
-    
-
-// function updateTable(search) {
-
-    
-//     d3.select("tbody").remove();
-    
-//     var tbody = d3.select("tbody"); 
-
-//     search.forEach((ufo) => {
-//         var row = tbody.append("tr");
-//         Object.entries(ufo).forEach(([key, value]) => {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         }); 
-//     });
-// };
-
-//init();
-
 
 
